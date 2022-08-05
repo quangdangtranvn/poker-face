6807d7749341c5f5db3415e203d7846b4cc91df2
quangdangtranvn <93366525+quangdangtranvn@users.noreply.github.com>
2022-08-05 17:15:33 +0700

    1.0

---
+++ unityWorkingFolder/Texas Holdem/Assets/Scripts/UI/CardView.cs
@@ -0,0 +1,51 @@
+using QTBlue.CardGame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+     public string name;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+    [SerializeField] private string suit;
+    [SerializeField] private float rankValue;
+    private Image img;
+    private Card currentCard;
+    private int childIndex;
+
+    public CardView(Card card)
+    {
+        this.currentCard = card;
+        this.name = card.name;
+        this.suit = card.suit;
+        this.rankValue = card.rankValue;
+    }
+    public int ChildIndex { get => childIndex; set => childIndex = value; }
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+
+    public void SetCardImg(Sprite sprite)
+    {
+        img.sprite = sprite;
+    }
+
+    public void SetCardInfo(Card card)
+    {
+        this.currentCard = card;
+        this.name = card.name;
+        this.suit = card.suit;
+        this.rankValue = card.rankValue;
+        this.img.sprite = card.image;
+    }
+
+    public void SetCardImg(Card card)
+    {
+        img.sprite = card.image;
+    }
+}
